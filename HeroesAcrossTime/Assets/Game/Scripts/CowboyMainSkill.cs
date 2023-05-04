43df57a358bf20e72fda0708607a954c1d5cd09f
Ege Sefertaş <egesefertas@gmail.com>
2023-05-04 23:44:28 +0300

    Cowboy Bases Made

---
+++ HeroesAcrossTime/Assets/Game/Scripts/CowboyMainSkill.cs
@@ -0,0 +1,15 @@
+
+public class CowboyMainSkill : CharacterSkillBase // basic revolver shooting
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ HeroesAcrossTime/Assets/Game/Scripts/CowboyMobilitySkill.cs
@@ -0,0 +1,15 @@
+
+public class CowboyMobilitySkill : CharacterSkillBase // rolling out of danger, has iFrames
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
