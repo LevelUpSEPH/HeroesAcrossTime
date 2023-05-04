87c0217f504b6c631ffde227504d7bca93bfc141
Ege Sefertaş <egesefertas@gmail.com>
2023-05-04 23:36:24 +0300

    Disabled Using Two Skills at Once

--- HeroesAcrossTime/Assets/Game/Scripts/CharacterSkillBase.cs
+++ HeroesAcrossTime/Assets/Game/Scripts/CharacterSkillBase.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CharacterSkillBase : MonoBehaviour
 {
     protected float _skillCooldown = 1f;
     private bool _readyToUse = true;
     
-    public virtual void UseSkill(Vector3 targetLocation){ // every skill needs a point to be used
+    public virtual void UseSkill(Vector3 targetLocation, Action onSkillUsed){ // cannot use two skills at once
         if(!_readyToUse)
             return;
         // use whatever skill this is
