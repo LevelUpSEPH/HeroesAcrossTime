ba94608c20fd7e75c2b229787ee5113510f35349
Ege Sefertaş <egesefertas@gmail.com>
2023-05-05 17:27:58 +0300

    BulletPoolController Added

---
+++ HeroesAcrossTime/Assets/Game/Scripts/BulletPoolController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPoolController : MonoBehaviour
+{
+    public static BulletPoolController Instance {get; private set;}
+    private List<GameObject> _bulletPool = new List<GameObject>();
+
+    public GameObject GetBulletToShoot(){
+        foreach (GameObject bullet in _bulletPool)
+        {
+            if(!bullet.activeinHierarchy)
+                return bullet;
+
+        }
+    }
+}
