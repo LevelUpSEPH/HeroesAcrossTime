3da1abe6bd5028ecde188b4f4027190df9336a06
Ege Sefertaş <egesefertas@gmail.com>
2023-05-05 17:28:12 +0300

    Cowboy Character Description Added

--- HeroesAcrossTime/Assets/Game/Scripts/CowboyCharacter.cs
+++ HeroesAcrossTime/Assets/Game/Scripts/CowboyCharacter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CowboyCharacter : PlayerCharacterBase
+public class CowboyCharacter : PlayerCharacterBase // high dps, low burst, medium mobility, no aoe
 {
     // Start is called before the first frame update
     void Start()
