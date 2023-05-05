9cac49114a64bb75e118c14383347227f10f40c2
Ege Sefertaş <egesefertas@gmail.com>
2023-05-05 17:29:01 +0300

    Updated CharacterSkillBase

--- HeroesAcrossTime/Assets/Game/Scripts/CharacterSkillBase.cs
+++ HeroesAcrossTime/Assets/Game/Scripts/CharacterSkillBase.cs
@@ -8,14 +8,15 @@ public class CharacterSkillBase : MonoBehaviour
     protected float _skillCooldown = 1f;
     private bool _readyToUse = true;
     
-    public virtual void UseSkill(Vector3 targetLocation, Action onSkillUsed){ // cannot use two skills at once
+    public virtual void UseSkill(Vector3 targetPosition, Action OnSkillUsed){ // cannot use two skills at once
         if(!_readyToUse)
             return;
         // use whatever skill this is
         StartCoroutine(StartSkillCooldown());
     }
 
-    private IEnumerator StartSkillCooldown(){
+    private IEnumerator StartSkillCooldown(Action OnCooldownComplete){
         yield return new WaitForSeconds(_skillCooldown);
+        OnCooldownComplete();
     }
 }
