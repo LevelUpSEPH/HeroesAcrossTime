cbcf1a92651d1800486b66415d9802aec6de96cf
Ege Sefertaş <egesefertas@gmail.com>
2023-05-10 16:50:21 +0300

    Changed Ultimate Concept

--- HeroesAcrossTime/Assets/Game/Scripts/CowboyUltimateSkill.cs
+++ HeroesAcrossTime/Assets/Game/Scripts/CowboyUltimateSkill.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class CowboyUltimateSkill : CharacterSkillBase // a ray goes from cowboy, picking enemies for a shot OR throws a dynamite
+public class CowboyUltimateSkill : CharacterSkillBase // shoots a high noon bullet, if it hit; the bullet insta kills and resets cooldown
 {
     // Start is called before the first frame update
     void Start()
