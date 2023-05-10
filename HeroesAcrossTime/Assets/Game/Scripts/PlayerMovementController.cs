feb455a9da92ba58cbc11ec782a437b2f91b2f89
Ege Sefertaş <egesefertas@gmail.com>
2023-05-10 14:19:01 +0300

    Refactor, LookRotation Implemented

--- HeroesAcrossTime/Assets/Game/Scripts/PlayerMovementController.cs
+++ HeroesAcrossTime/Assets/Game/Scripts/PlayerMovementController.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     private CharacterController _characterController;
+    private PlayerCharacterBase _playerCharacter;
+    private GameObject _activePlayerModel;
     private float _movementSpeed = 5f;
 
     private void Start(){
-        _characterController = GetComponent<CharacterController>();
+        Initialize();
+        AddListeners();
+    }
+
+    private void OnDisable(){
+        RemoveListeners();
     }
 
     private void Update(){
         HandleMovement();
+        HandleRotation();
+    }
+
+    private void Initialize(){
+        _characterController = GetComponent<CharacterController>();
+        _playerCharacter = ActiveCharacterController.Instance.GetActivePlayerCharacterBase();
+        _activePlayerModel = _playerCharacter.gameObject;
+    }
+    private void AddListeners(){
+        ActiveCharacterController.SwitchedCharacter += OnSwitchedCharacter;
+    }
+
+    private void RemoveListeners(){
+        ActiveCharacterController.SwitchedCharacter -= OnSwitchedCharacter;
     }
 
     private void HandleMovement(){
@@ -20,4 +41,12 @@ public class PlayerMovementController : MonoBehaviour
         _characterController.Move(movementDir * _movementSpeed * Time.deltaTime);
     }
 
+    private void HandleRotation(){
+        _activePlayerModel.transform.LookAt(MouseWorldPositionController.Instance.GetCursorPosTransform(), Vector3.up);
+    }
+
+    private void OnSwitchedCharacter(PlayerCharacterBase playerCharacterBase){
+        _playerCharacter = playerCharacterBase;
+        _activePlayerModel = _playerCharacter.gameObject;
+    }
 }
