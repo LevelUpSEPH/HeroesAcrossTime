69d02ea847376bf5dbeefde568b338341afd4f23
Ege Sefertaş <egesefertas@gmail.com>
2023-05-10 14:18:42 +0300

    Added Global Skill Cooldowns

--- HeroesAcrossTime/Assets/Game/Scripts/CharacterSkillBase.cs
+++ HeroesAcrossTime/Assets/Game/Scripts/CharacterSkillBase.cs
@@ -6,17 +6,22 @@ using System;
 public class CharacterSkillBase : MonoBehaviour
 {
     protected float _skillCooldown = 1f;
-    private bool _readyToUse = true;
+    protected bool _readyToUse = true;
     
-    public virtual void UseSkill(Vector3 targetPosition, Action OnSkillUsed){ // cannot use two skills at once
+    public virtual void UseSkill(Vector3 targetPosition, Action OnSkillUsed){ 
         if(!_readyToUse)
             return;
         // use whatever skill this is
         StartCoroutine(StartSkillCooldown());
+        StartCoroutine(GlobalSkillCooldown(OnSkillUsed)); // cooldown before using another skill
     }
 
-    private IEnumerator StartSkillCooldown(Action OnCooldownComplete){
+    protected IEnumerator StartSkillCooldown(){
         yield return new WaitForSeconds(_skillCooldown);
+    }
+
+    protected IEnumerator GlobalSkillCooldown(Action OnCooldownComplete){
+        yield return new WaitForSeconds(0.5f);
         OnCooldownComplete();
     }
 }
