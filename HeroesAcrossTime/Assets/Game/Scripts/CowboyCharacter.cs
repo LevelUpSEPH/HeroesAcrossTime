64b3aed790e46ebc654f3e46103f1b4125f0b8b5
Ege Sefertaş <egesefertas@gmail.com>
2023-05-12 01:33:04 +0300

    Folder Organization

--- HeroesAcrossTime/Assets/Game/Scripts/CowboyCharacter.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CowboyCharacter : PlayerCharacter // high dps, low burst, medium mobility, no aoe
-{
-
-}
